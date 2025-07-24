8cbe4a178ce113cfc5562df3dfbb229c722ff5f0
jessek-dev <jesse.kedy@gmail.com>
2025-07-24 19:42:17 -0400

    Create config.js

---
+++ manage/config.js
@@ -0,0 +1,9 @@
+// File: /manage/config.js
+const CONFIG = {
+  // Your Google Apps Script Web App URL
+  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbzLLiYLeg82SMieWyCB6iJlXRKTzoRb_omJBhZhwGw0S8Si1MNe7kLwRpsFmS9bT_4/exec',
+  
+  // App settings
+  APP_NAME: 'Dad Jokes Manager',
+  VERSION: '1.0.0'
+};
