305cf106417115c1369e24b1655c264fbc71794f
jessek-dev <jesse.kedy@gmail.com>
2025-07-24 20:32:12 -0400

    Update config.js

--- manage/config.js
+++ manage/config.js
@@ -1,7 +1,7 @@
 // File: /manage/config.js
 const CONFIG = {
   // Your NEW Google Apps Script Web App URL
-  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycby0b300vXFvYLmwbTGGWYe4QUiFvA8g7NYKNWFSQTV0zmlW1E6V7notE7vL0zZ5Znew/exec',
+  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbxlWZI8n0FBiiv3_zERRF0Ln30wLnJzoBkCU8aN7WxT1OnLqbqP9lmaFDQhxj82SbwJ/exec',
   
   // App settings
   APP_NAME: 'Dad Jokes Manager',
